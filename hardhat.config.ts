b3cfe37478c06e4f79336dc6c986fe4cd60fbe16
byma <git@by-ma.art>
2021-01-16 13:46:42 +0000

    Deploy & Verify command.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import {task} from "hardhat/config";
 require("@nomiclabs/hardhat-waffle");
 import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 
 task("accounts", "Prints the list of accounts", async () => {
   const {ethers} = await import('hardhat');
@@ -8,6 +9,16 @@ task("accounts", "Prints the list of accounts", async () => {
   for (const account of accounts) {
     console.log(account.address);
   }
+});
+
+
+task("deploy", "Deploy the contract", async () => {
+  const {ethers} = await import('hardhat');
+  const MahinNFT = await ethers.getContractFactory("MahinNFT");
+  const nft = await MahinNFT.deploy();
+  await nft.deployed();
+  console.log("NFT deployed to:", nft.address);
+  console.log(`Verify with: npx hardhat verify --network ... ${nft.address}`);
 });
 
 task("init-tokens", "Call the contract to init a directory of input tokens", async () => {
