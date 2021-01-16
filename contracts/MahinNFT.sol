e2b6226f7bfc5dbdcec6598bfbbb1159a3fba1fc
byma <git@by-ma.art>
2021-01-16 12:25:38 +0000

    Experiment with storing SVGs as events.

--- contracts/MahinNFT.sol
+++ contracts/MahinNFT.sol
@@ -6,14 +6,33 @@ import "./ERC721.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract MahinNFT is ERC721("Mahin", "MAHIN"), Ownable {
+  event TokenDataStorage(
+    uint256 indexed _tokenId,
+    string[] states
+);
+
   struct Piece {
     string[] states;
     string[] ipfsHashes;
     uint8 currentState;
   }
 
+  // TODO: withdraw functions
+
+  uint public constant projectRuntimeSeconds = 365 days * 5;   // Runs for 5 years
+  uint public constant targetProbability     = 2000000000000000;  // 0.2% - over the course of the project
+  uint public constant denominator           = 10000000000000000; // 100%
+  // This has been pre-calculated based on the values above
+  uint public constant probabilityPerSecond  = 14151671;        // 0.0000000014151671%
+
   mapping(uint256 => Piece) pieces;
-  uint256 randomTargetBlock = 0;
+  uint256 randomSeedBlock = 0;
+  int128 public rollProbability = 0;
+  uint32 public lastRollTime = 0;
+
+  constructor() public {
+    lastRollTime = uint32(block.timestamp);
+  }
 
   // Returns the current SVG of the piece.
   function getSvg(uint256 tokenId) public view returns (string memory) {
@@ -24,10 +43,11 @@ contract MahinNFT is ERC721("Mahin", "MAHIN"), Ownable {
   // Will be used by the owner during setup to create all pieces of the work, before renouncing ownership.
   // states - the svg code for each state.
   // ipfsHashes - the ipfs location of each state - needed so provided an off-chain metadata url.
-  function initToken(uint256 tokenId, string[] memory states, string[][] memory ipfsHashes) public onlyOwner {
+  function initToken(uint256 tokenId, string[] memory states, string[] memory ipfsHashes) public onlyOwner {
     pieces[tokenId].states = states;
     pieces[tokenId].ipfsHashes = ipfsHashes;
     pieces[tokenId].currentState = 0;
+    emit TokenDataStorage(tokenId, states);
   }
 
   // Allow contract owner to set the IPFS host
