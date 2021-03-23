af9838240184df371d5a1ef9fc9e4f49cdc9d92d
byma <git@by-ma.art>
2021-03-23 12:24:26 +0000

    Add plausible.

--- site/lib/Layout.tsx
+++ site/lib/Layout.tsx
@@ -14,6 +14,7 @@ export function Layout(props: {
     <Head>
       <title>Project Mahin</title>
       <link rel="icon" href="/favicon.ico" />
+      {process.browser && <script defer async data-domain="mahin.by-ma.art" src="https://plausible.io/js/plausible.js" />}
     </Head>
 
     {!props.hideHeader ? <Header /> : null}
